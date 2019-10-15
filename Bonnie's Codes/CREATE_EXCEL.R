518f51cfeaa37ec6714baf4b9cf55f36861dd97a
Bonnie Lin <b2lin@ucsd.edu>
2019-10-15 16:22:13 -0700

    Excel conversion in preparation for discrepency plots

---
+++ Bonnie's Codes/CREATE_EXCEL.R
@@ -0,0 +1,24 @@
+setwd("~/Dropbox (Palmer Lab)/Palmer Lab/Bonnie Lin/Tom_Jhou_U01DA044468_Dropbox_copy")
+
+# Load libraries
+library(tidyverse) #loads dplyr, tidyr, ggplot2, purrr, etc
+require(mgsub)
+library(readxl)
+library(lubridate)
+library(openxlsx)
+library(stringr)
+library(data.table)
+
+u01.importxlsx <- function(xlname){
+  path_sheetnames <- excel_sheets(xlname)
+  df <- lapply(excel_sheets(path = xlname), read_excel, path = xlname)
+  names(df) <- path_sheetnames
+  return(df)
+} 
+
+Jhou_Excel <- u01.importxlsx("U01 Master sheet_readonly.xlsx")
+
+### RUNWAY
+Jhou_Runway <- Jhou_Excel[["Runway"]] %>% as.data.table
+Jhou_Runway_test <- Jhou_Runway[1:16, ]
+Jhou_Runway_test <- rbindlist(list(Jhou_Runway_test, as.list(names(Jhou_Runway))), fill=FALSE) # retain the column names before transposing
