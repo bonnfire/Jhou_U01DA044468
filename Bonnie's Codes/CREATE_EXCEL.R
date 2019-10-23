e5395e76bcefea4000060a17da6d9d46383cf4b3
Bonnie Lin <b2lin@ucsd.edu>
2019-10-23 16:42:23 -0700

    Creating datasets from Runway excel sheet, wide format for both reversals and elapsed time information

--- Bonnie's Codes/CREATE_EXCEL.R
+++ Bonnie's Codes/CREATE_EXCEL.R
@@ -18,7 +18,39 @@ u01.importxlsx <- function(xlname){
 
 Jhou_Excel <- u01.importxlsx("U01 Master sheet_readonly.xlsx")
 
-### RUNWAY
+################################
+########### Runway #############
+################################
+
 Jhou_Runway <- Jhou_Excel[["Runway"]] %>% as.data.table
 Jhou_Runway_test <- Jhou_Runway[1:16, ]
 Jhou_Runway_test <- rbindlist(list(Jhou_Runway_test, as.list(names(Jhou_Runway))), fill=FALSE) # retain the column names before transposing
+Jhou_Runway[, names(Jhou_Runway) := lapply(.SD, as.character)] # preserve values by turning into characters before transposing 
+
+
+tJhou_Runway <- data.table::transpose(Jhou_Runway) # transpose data
+colnames(tJhou_Runway) <- as.character(tJhou_Runway[1,])
+tJhou_Runway <- tJhou_Runway[-1,] # data.table cannot delete rows by reference with tJhou_Runway[1 := NULL,]  
+
+# append reversals to column names
+reversalstartindex <- which(grepl("^Hab", colnames(tJhou_Runway)))[3]
+reversalendindex <- which(grepl("^Coc.*12$", colnames(tJhou_Runway)))[2]
+
+colnames(tJhou_Runway)[reversalstartindex:reversalendindex] <- paste0(colnames(tJhou_Runway)[reversalstartindex:reversalendindex], "Reversals") # append reversals
+
+
+# separate reversals from elapsed time information
+
+tJhou_Runway_vars_nonrevers <- grep("^(Gender|Habituation \\d|Coc|Animal)", colnames(tJhou_Runway), value = T) %>% 
+  grep('\\d+(?!Reversals)$', . , value = T, perl=T)
+tJhou_Runway_nonreverswide <- tJhou_Runway[, tJhou_Runway_vars_nonrevers, with=FALSE]
+tJhou_Runway_nonreverswide[, animalid := names(Jhou_Runway)[-1]] # remove animal id element but retain the animal ids for the data
+
+tJhou_Runway_vars_reversals <- grep("^(Gender|Habituation \\d|Coc|Animal)", colnames(tJhou_Runway), value = T) %>% 
+  grep('\\d+(?=Reversals)', . , value = T, perl=T)
+tJhou_Runway_reverswide <- tJhou_Runway[, tJhou_Runway_vars_reversals, with=FALSE]
+tJhou_Runway_reverswide[, animalid := names(Jhou_Runway)[-1]] # remove animal id element but retain the animal ids for the data
+
+# convert wide to long formats for both reversals and elapsed time datasets 
+
+tJhou_Runway_long <- gather(tJhou_Runway_wide, session, elapsedtime, control:cond2, factor_key=TRUE)
